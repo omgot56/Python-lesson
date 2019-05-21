a80aa9a45ea6ae002d860d84dc6b5d67837ccbea
omgot56 <37599033+omgot56@users.noreply.github.com>
2019-05-21 17:34:57 +0300

    Update less2.py

--- less2.py
+++ less2.py
@@ -1,6 +1,12 @@
-def odometer (N):
+def odometer(N):
     oksana = 0
+    now_time = 0
     for i in range (len(N)):
-        if i == 0 or i%2 == 0:
-            oksana += N[i]
-    return oksana
+        if i == 0:
+            now_time = N[i+1]
+            oksana += now_time*N[i]
+        elif i > 1 and i % 2 == 0:
+            now_time = N[i-1]
+            now_time =  N[i+1] - now_time
+            oksana += now_time * N[i]
+    return(oksana)
